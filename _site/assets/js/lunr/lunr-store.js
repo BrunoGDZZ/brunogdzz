51396690bfa9a3b749a550018996c10c5238fc14
BrunoGDZZ <bruno.garbuio.souza@gmail.com>
2025-03-02 10:35:44 -0600

    Removed _site from repository

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Intro Blog",
-        "excerpt":"Welcome to My Blog! 👾 Heyo! My name is Bruno and this is probably where I’m going to speel on and on about things I find interesting in the field of computer science. What to Expect Here’s what you can look forward to: Tech &amp; Coding – Projects I’m working...","categories": [],
-        "tags": [],
-        "url": "/2025/02/03/title.html",
-        "teaser": null
-      }]
